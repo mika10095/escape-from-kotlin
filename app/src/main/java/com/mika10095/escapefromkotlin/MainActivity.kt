d7052510d12f98f3480752350ced78994150547d
mika10095 <csml.csdsz@gmail.com>
2026-03-03 22:43:49 +0100

    initial

---
+++ app/src/main/java/com/mika10095/escapefromkotlin/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.mika10095.escapefromkotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.graphics.*
+import android.view.SurfaceView
+class MainActivity : AppCompatActivity() {
+    lateinit var screen: SurfaceView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        screen = findViewById(R.id.gameView)
+        screen.setBackgroundColor(Color.RED)
+
+    }
+}
